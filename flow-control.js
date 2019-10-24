e0ae424896b2c74b4dbfed8e88741aa1893f50f2
Julia Miller <ihre.hexenhafte.hoheit@gmail.com>
2019-10-24 12:06:40 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -10,7 +10,7 @@ function teenager(age) {
 function ageChecker(age) {
   if(age>=13  && age<=19) return "You are a teenager!";
   else if(age<13) return "You are a kid";
-  else if(age>19) return 
+  else if(age>19) return "You are a grownup";
 }
 
 function ternaryTeenager(age) {
